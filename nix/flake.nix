69b318bece3259d158d26e5e773b33aae301e84b
Marcello Evangelista <marc.torden@gmail.com>
2025-03-17 21:35:41 +0100

    chore: update nix-homebrew setup

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,12 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-    homebrew-core.url = "github:Homebrew/homebrew-core";
-    homebrew-cask.url = "github:Homebrew/homebrew-cask";
-    homebrew-bundle.url = "github:Homebrew/homebrew-bundle";
   };
 
-  outputs = { self, nixpkgs, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle }: 
+  outputs = { self, nixpkgs, nix-darwin, nix-homebrew}: 
     let
       system = "aarch64-darwin";  # Change to "x86_64-darwin" for Intel Macs
       pkgs = import nixpkgs { inherit system; };
@@ -27,14 +24,6 @@
               enable = true;  # Enable nix-homebrew management
               enableRosetta = true;  # Ensure Rosetta 2 support for x86_64 packages
               user = "calliop3";  # Set correct user for managing Homebrew
-
-              # Optional: Declarative tap management
-              taps = {
-                "homebrew/homebrew-core" = homebrew-core;
-                "homebrew/homebrew-cask" = homebrew-cask;
-                "homebrew/homebrew-bundle" = homebrew-bundle;
-              };
-
               # Optional: Enable fully-declarative tap management
               mutableTaps = false;
             };
