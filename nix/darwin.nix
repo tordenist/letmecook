e440b75a10a84bf8f0bab9f4e98c031edbc2af73
Marcello Evangelista <calliop3@Marcellos-MacBook-Pro.local>
2025-03-18 19:10:44 +0100

    chore: cleaning up darwin settings

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -6,51 +6,58 @@
   # Set nix build user group ID to avoid UID mismatch errors
   ids.gids.nixbld = 350;
 
-  # Set macOS system preferences
-  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true; # Show file extensions
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false; # Disable press-and-hold for key repeats
-  system.defaults.NSGlobalDomain.KeyRepeat = 2; # Set key repeat speed
-  system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark"; # Enable dark mode by default
-  system.defaults.NSGlobalDomain.NSWindowResizeTime = 0.001; # Faster window resizing
-
-  # Disable guest account
-  system.defaults.loginwindow.GuestEnabled = false;
-
-  # Set Finder preferences
-  system.defaults.finder.FXPreferredViewStyle = "clmv"; # Use column view in Finder
-  system.defaults.finder.ShowPathbar = true; # Show path bar in Finder
-  system.defaults.finder.AppleShowAllFiles = true; # Show hidden files in Finder
-
-  # Set screenshot location
-  system.defaults.screencapture.location = "$HOME/grimorium/Screenshots";
-
-  # Disable "Are You Sure You Want to Open This App?" warning
-  system.defaults.LaunchServices.LSQuarantine = false;
-
-  # Add grimorium folder to Finder favorites
-  system.activationScripts.addFinderFavorite.text = ''
-    osascript -e "tell application \"Finder\" to make new alias at POSIX file \"$HOME\" to POSIX file \"$HOME/grimorium\" with properties {name:\"grimorium\"}"
-  '';
-
-  # Set a specific wallpaper
+  # Set specific wallpaper
   system.activationScripts.setWallpaper.text = ''
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$HOME/grimorium/letmecook/wallpaper/muses.jpeg\""
-  '';
+  osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/grimorium/letmecook/wallpaper/muses.jpeg"'
+'';
 
-  # Configure Dock preferences
-  system.defaults.dock.autohide = true; # Automatically hide the dock
-  system.defaults.dock.static-only = true; # Only show pinned apps
-  system.activationScripts.setDockApps.text = ''
-    defaults write com.apple.dock persistent-apps -array \
-      '{tile-data={file-data={_CFURLString="/Applications/Ghostty.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/Applications/Spotify.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/Applications/Postman.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/Applications/Zen Browser.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/Applications/Obsidian.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/Applications/Slack.app"; _CFURLStringType=15;};};}' \
-      '{tile-data={file-data={_CFURLString="/System/Applications/Calendar.app"; _CFURLStringType=15;};};}'
-    killall Dock
-  '';
+  # Set macOS system preferences
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;  # Show file extensions
+      ApplePressAndHoldEnabled = false;  # Disable press-and-hold for key repeats
+      KeyRepeat = 2;  # Set key repeat speed
+      AppleInterfaceStyle = "Dark";  # Enable dark mode by default
+      NSWindowResizeTime = 0.001;  # Faster window resizing
+    };
+
+    loginwindow = {
+      GuestEnabled = false;  # Disable guest account
+    };
+
+    controlcenter = {
+      BatteryShowPercentage = true;
+      Sound = true;
+    };
+
+    finder = {
+      FXPreferredViewStyle = "clmv";  # Use column view in Finder
+      ShowPathbar = true;  # Show path bar in Finder
+      AppleShowAllFiles = true;  # Show hidden files in Finder
+    };
+
+    screencapture = {
+      location = "$HOME/grimorium/Screenshots";  # Set screenshot location
+    };
+
+    LaunchServices = {
+      LSQuarantine = false;  # Disable "Are You Sure You Want to Open This App?" warning
+    };
+
+    dock = {
+      autohide = true;  # Automatically hide the dock
+      static-only = true;  # Only show pinned apps
+      persistent-apps = [
+        "/Applications/Ghostty.app"
+        "/Applications/Spotify.app"
+        "/Applications/Postman.app"
+        "/Applications/Zen Browser.app"
+        "/Applications/Obsidian.app"
+        "/Applications/Slack.app"
+        "/System/Applications/Calendar.app"
+      ];
+    };
+  };
 
   # Use Zsh as the default shell
   programs.zsh.enable = true;
@@ -58,3 +65,4 @@
   # State version
   system.stateVersion = 4;
 }
+
