3705c84c4c29c17c75da44f110d04614ef31cd33
Marcello Evangelista <calliop3@Marcellos-MacBook-Pro.local>
2025-03-25 10:44:53 +0100

    chore: add missing packages

--- nix/homebrew.nix
+++ nix/homebrew.nix
@@ -4,15 +4,15 @@
     onActivation.cleanup = "zap"; # Fully remove unused packages
     brews = [
       "act" "amass" "antidote" "bat" "btop" "dnstwist" "eza" "feroxbuster" "fzf"
-      "gh" "git" "glow" "hey" "httpie" "john-jumbo" "jq" "lazygit"
+      "gh" "git" "glow" "grype" "hey" "httpie" "john-jumbo" "jq" "lazygit"
       "lefthook" "libpcap" "masscan" "mise" "mtr" "neofetch" "neovim"
       "nmap" "pre-commit" "procs" "ripgrep" "ruff" "scorecard" "shellcheck"
-      "sops" "starship" "stow" "thefuck" "theharvester" "tldr" "tmux" "tree" "yq"
+      "sops" "starship" "stow" "syft" "thefuck" "theharvester" "tldr" "tmux" "tree" "yq"
       "zoxide"
     ];
     casks = [
-      "amethyst" "font-hack-nerd-font" "font-mononoki-nerd-font" "ghostty"
-      "google-chrome" "google-cloud-sdk" "kitty" "obsidian" "postman"
+      "amethyst" "docker" "font-hack-nerd-font" "font-mononoki-nerd-font" "ghostty"
+      "google-chrome" "google-cloud-sdk" "kitty" "obsidian" "podman-desktop" "postman"
       "slack" "spotify" "stats" "zen-browser" "1password" "1password-cli"
     ];
     taps = [ "caffix/amass" "fluxcd/tap" "homebrew/bundle" "jesseduffield/lazygit" ];
